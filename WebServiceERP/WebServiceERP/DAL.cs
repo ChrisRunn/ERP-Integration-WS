3569ca3bae67eb4b996350c3c4cb5c02e62d5e51
ChrisRunn <christian.runnstrom@gmail.com>
2014-04-22 13:43:59 +0200

    first

---
+++ WebServiceERP/WebServiceERP/DAL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceERP
+{
+    public class DAL
+    {
+        string connectionString = "server=localhost; Trusted_Connection=yes; database=Demo Database NAV (5-0);";
+
+        public DataSet GetAllEmployees()
+        {
+
+            SqlDataAdapter adapter = new SqlDataAdapter(
+     "select No_ from [CRONUS Sverige AB$Employee]", connectionString);
+
+            DataSet studentDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(studentDS, "Students");
+
+            return studentDS;
+
+        }
+    }
+}
---
+++ WebServiceERP/WebServiceERP/Service1.asmx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebServiceERP
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        DAL dal = new DAL();
+
+        [WebMethod(Description = "Inserts new employee", EnableSession = false)]
+        public void InsertEmployee()
+        {
+        }
+
+        [WebMethod(Description = "Updates employee", EnableSession = false)]
+        public void UpdateEmployee()
+        {
+        }
+
+        [WebMethod(Description = "Deletes employee", EnableSession = false)]
+        public void DeleteEmployee()
+        {
+        }
+
+        [WebMethod(Description = "Shows all employees", EnableSession = false)]
+        public DataSet GetAllEmployees()
+        {
+            return dal.GetAllEmployees();
+        }
+
+
+    }
+}
