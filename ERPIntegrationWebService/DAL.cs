0fd35ae447c357331f593b1945c7d30d169b82e2
ChrisRunn <christian.runnstrom@gmail.com>
2014-04-23 14:39:55 +0200

    FUngerande

--- ERPIntegrationWebService/DAL.cs
+++ ERPIntegrationWebService/DAL.cs
@@ -4,25 +4,203 @@ using System.Linq;
 using System.Web;
 using System.Data.SqlClient;
 using System.Data;
+using System.Diagnostics;
 
 namespace ERPIntegrationWebService
 {
     public class DAL
     {
         string connectionString = "server=localhost; Trusted_Connection=yes; database=Demo Database NAV (5-0);";
-
-        public DataSet GetAllEmployees()
+        #region GENERISK METOD
+        private void ExecuteUpdate(string sqlStr)
         {
+            SqlConnection con = new SqlConnection(connectionString);
+            Debug.WriteLine(sqlStr);
+            try
+            {
+                con.Open();
+                SqlCommand com = new SqlCommand(sqlStr, con);
+                com.ExecuteNonQuery();
 
-            SqlDataAdapter adapter = new SqlDataAdapter(
-     "select No_ from [CRONUS Sverige AB$Employee]", connectionString);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+        }
+        private DataTable ExecuteQuery(string sqlStr)
+        {
+            DataTable dataTable = new DataTable();
+            SqlConnection con = new SqlConnection(connectionString);
 
-            DataSet employeeDS = new DataSet();
-            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
-            adapter.Fill(employeeDS, "Emplyees");
+            try
+            {
+                con.Open();
+                SqlDataAdapter dataAdapter = new SqlDataAdapter(sqlStr, con);
+                dataAdapter.Fill(dataTable);
+            }
 
-            return employeeDS;
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+            }
+            return dataTable;
+        }
+        #endregion GENERISK METOD
+        #region Select, Delete, Update, Insert
+        public void InsertEmployee(string no, string name)
+        {
+            string sqlStr = "INSERT INTO [CRONUS Sverige AB$Employee Absence] VALUES (DEFAULT, '" + no + "', '" + name + "' , '2004-01-02 00:00:00.000', '1753-01-01 00:00:00.000', '0', '0', '0', '0', '0', '0')";
+            ExecuteUpdate(sqlStr);
 
         }
+        public void UpdateEmployee(string no, string name, string lastName)
+        {
+            string sqlStr = "UPDATE [CRONUS Sverige AB$Employee] SET [First Name] = '" + name + "', [Last Name] = '" + lastName + "' WHERE No_ = '" + no + "'";
+            ExecuteUpdate(sqlStr);
+
+        }
+        public void DeleteEmployee(string no)
+        {
+            string sqlStr = "DELETE FROM [CRONUS Sverige AB$Employee] WHERE No_ = '" + no + "'";
+            ExecuteUpdate(sqlStr);
+
+        }
+        public DataTable SearchEmployee(string searchString)
+        {
+
+            string sqlStr = "SELECT * FROM [CRONUS Sverige AB$Employee] where No_ like '%" + searchString + "%' or [First Name] like '%" + searchString + "%' or [Last Name] like '%" + searchString + "%'";
+            DataTable dt = ExecuteQuery(sqlStr);
+            return dt;
+        }
+
+        #endregion Select, Delete, Update, Insert
+
+        #region Uppgift A
+
+        public DataSet GetEmployeeAndMetaData()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "select * from dbo.sysobjects where name like 'CRONUS Sverige AB$Employee%' and xtype = 'U'", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetSickEmployee()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "select [First Name], [Last Name] from [CRONUS Sverige AB$Employee] e, [CRONUS Sverige AB$Employee Absence] a where a.[Employee No_] = e.[No_] and Description = 'Sjuk' group by [First Name], [Last Name]", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetMostSickEmployee()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "select max(e.[First Name]) as Name from [CRONUS Sverige AB$Employee] e, [CRONUS Sverige AB$Employee Absence] a where a.[Employee No_] = e.[No_] and Description = 'Sjuk' group by [First Name], [Last Name]", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetEmployeeAndRelatives()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "select e.[First Name], e.[Last Name], l.[Relative Code], l.[First Name] from [CRONUS Sverige AB$Employee Relative] l, [CRONUS Sverige AB$Employee] e where l.[Employee No_] = e.[No_]", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        #endregion Uppgift A
+
+        #region Uppgift B
+        public DataSet GetAllKeys()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "SELECT TOP 100 [id], [xtype], [name] FROM sysobjects WHERE xtype = 'F' OR xtype = 'PK'", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+
+        public DataSet GetAllIndexes()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "SELECT TOP 100 [id], [status] FROM sysindexes", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetAllConstraints()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+           "SELECT TOP 100 [constid], [id] FROM sysconstraints", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetAllTables()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+           "SELECT TOP 100 [name] FROM sysobjects WHERE xtype = 'U'", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetAllTables2()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "SELECT TOP 100 [name] FROM sys.tables", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetColumnsEmployee()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "SELECT [name], [id], [xtype] FROM syscolumns WHERE id = object_id('[CRONUS Sverige AB$Employee]')", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+        public DataSet GetColumnsEmployee2()
+        {
+            SqlDataAdapter adapter = new SqlDataAdapter(
+            "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'CRONUS Sverige AB$Employee'", connectionString);
+            DataSet employeeDS = new DataSet();
+            adapter.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            adapter.Fill(employeeDS, "Employees");
+
+            return employeeDS;
+        }
+
+        #endregion Uppgift B
+
+       
     }
 }
--- ERPIntegrationWebService/ERPIntegrationWS.asmx.cs
+++ ERPIntegrationWebService/ERPIntegrationWS.asmx.cs
@@ -17,28 +17,106 @@ namespace ERPIntegrationWebService
     // [System.Web.Script.Services.ScriptService]
     public class ERPIntegrationWS : System.Web.Services.WebService
     {
-
         DAL dal = new DAL();
 
+        #region Select, Update, Delete, Insert
         [WebMethod(Description = "Inserts new employee", EnableSession = false)]
-        public void InsertEmployee()
+        public void InsertEmployee(string no, string name)
         {
+            dal.InsertEmployee(no, name);
         }
 
         [WebMethod(Description = "Updates employee", EnableSession = false)]
-        public void UpdateEmployee()
+        public void UpdateEmployee(string no, string name, string lastName)
         {
+            dal.UpdateEmployee(no, name, lastName);
         }
 
         [WebMethod(Description = "Deletes employee", EnableSession = false)]
-        public void DeleteEmployee()
+        public void DeleteEmployee(string no)
         {
+            dal.DeleteEmployee(no);
         }
 
-        [WebMethod(Description = "Shows all employees", EnableSession = false)]
-        public DataSet GetAllEmployees()
+        [WebMethod(Description = "Shows a specific employee", EnableSession = false)]
+        public void SearchEmployee(string searchString)
         {
-            return dal.GetAllEmployees();
+            dal.SearchEmployee(searchString);
         }
+
+        #endregion Select, Update, Delete, Insert
+
+        #region Uppgift A
+
+        [WebMethod(Description = "Shows all employees and metadata", EnableSession = false)]
+        public DataSet GetEmployeeAndMetaData()
+        {
+            return dal.GetEmployeeAndMetaData();
+        }
+
+        [WebMethod(Description = "Shows all employees and relatives", EnableSession = false)]
+        public DataSet GetEmployeeAndRelatives()
+        {
+            return dal.GetEmployeeAndRelatives();
+        }
+
+        [WebMethod(Description = "Shows all sick employees", EnableSession = false)]
+        public DataSet GetSickEmployee()
+        {
+            return dal.GetSickEmployee();
+        }
+
+        [WebMethod(Description = "Shows most sick employees", EnableSession = false)]
+        public DataSet GetMostSickEmployee()
+        {
+            return dal.GetMostSickEmployee();
+        }
+        #endregion Uppgift A
+
+        #region Uppgift B
+
+        [WebMethod(Description = "Shows all Keys", EnableSession = false)]
+        public DataSet GetAllKeys()
+        {
+            return dal.GetAllKeys();
+        }
+
+        [WebMethod(Description = "Shows all indexes", EnableSession = false)]
+        public DataSet GetAllIndexes()
+        {
+            return dal.GetAllIndexes();
+        }
+
+        [WebMethod(Description = "Shows all constraints", EnableSession = false)]
+        public DataSet GetAllConstraints()
+        {
+            return dal.GetAllConstraints();
+        }
+
+        [WebMethod(Description = "Shows all tables solution 1", EnableSession = false)]
+        public DataSet GetAllTables()
+        {
+            return dal.GetAllTables();
+        }
+
+        [WebMethod(Description = "Shows all tables solution 2", EnableSession = false)]
+        public DataSet GetAllTables2()
+        {
+            return dal.GetAllTables2();
+        }
+
+        [WebMethod(Description = "Shows all columns in Employee table Solution 1", EnableSession = false)]
+        public DataSet GetColumnsEmployee()
+        {
+            return dal.GetColumnsEmployee();
+        }
+
+        [WebMethod(Description = "Shows all columns in Employee table Solution 2", EnableSession = false)]
+        public DataSet GetColumnsEmployee2()
+        {
+            return dal.GetColumnsEmployee2();
+        }
+        #endregion Uppgift A
+
     }
 }
