bce2e6c02e6cb5bdca29f39651b040233ecf4029
William Svedström <william.svedstrom@me.com>
2014-04-24 13:54:32 +0200

    Employee

---
+++ ERPIntegrationWebService/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERPIntegrationWebService
+{
+    public class Employee
+    {
+        private string firstName;
+        private string lastName;
+
+        public Employee() { }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+    }
+}
